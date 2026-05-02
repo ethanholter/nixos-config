171021439ba5a7aedb1c97dc3660c3a06c0ca39b
Ethan Holter <holterethan@gmail.com>
2026-05-02 11:50:11 -0500

    odrive udev rules

--- flake.nix
+++ flake.nix
@@ -7,9 +7,10 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    odrive-udev.url = "github:ethanholter/odrive-udev";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, odrive-udev, ... }: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -17,9 +18,9 @@
           ./configuration.nix
           ./devices/desktop.nix
           home-manager.nixosModules.home-manager
+          odrive-udev.nixosModules.default
         ];
       };
-
       thinkpad = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
